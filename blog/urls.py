4a74a1a078df38df2749e5bfcf747a62134d4883
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-09-28 20:46:16 +0200

    Display a single article by id && List all articles of a specific month

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home),
+    url(r'^article/(?P<id_article>\d+)$', views.view_article),
+    url(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})$', views.list_articles),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,11 @@ def home(request):
     text = """<h1>Bienvenue sur le site des crepes bretonnes</h1>
               <p>Les crepes bretonnes ca tue des mouettes en plein vol</p>"""
     return HttpResponse(text)
+
+def view_article(request, id_article):
+    text = "Vous avez demandé l'article {0}".format(id_article)
+    return HttpResponse(text)
+
+def list_articles(request, year, month):
+    text = "Vous avez demandé la liste des articles du mois {0}/{1}".format(month, year)
+    return HttpResponse(text)
