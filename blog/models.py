351d6559adf531a7640a2e994c2d0de3308571ab
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-09-30 19:20:12 +0200

    Righten field type typo

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=42)
     content = models.TextField(null=True)
-    date = models.DateField(auto_now_add=True, auto_now=False, verbose_name='Date de parution')
+    date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name='Date de parution')
 
     def __str__(self):
         """Méthode facilitant la reconnaissance et le traitement de cet objet"""
