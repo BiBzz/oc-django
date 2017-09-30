33e995696feda6b95a8776253a9e7f7693b26c60
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-09-30 19:32:34 +0200

    Implement blog categories

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,14 @@ class Article(models.Model):
     author = models.CharField(max_length=42)
     content = models.TextField(null=True)
     date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name='Date de parution')
+    category = models.ForeignKey('Category')
 
     def __str__(self):
         """Méthode facilitant la reconnaissance et le traitement de cet objet"""
         return self.title 
+
+class Category(models.Model):
+    label = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.label
