5af906b92083df02f854ff72b943dee7e7aaf0fc
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-09-30 02:15:38 +0200

    Create Article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=42)
+    content = models.TextField(null=True)
+    date = models.DateField(auto_now_add=True, auto_now=False, verbose_name='Date de parution')
+
+    def __str__(self):
+        """Méthode facilitant la reconnaissance et le traitement de cet objet"""
+        return self.title 
