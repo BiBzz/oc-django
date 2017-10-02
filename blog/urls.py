fc30aaa0fd285dbcde174c8852543cfdfa71e567
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-10-02 21:04:21 +0200

    Display articles list on homepage

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^article/(?P<id_article>\d+)$', views.view_article),
-    url(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})$', views.list_articles),
-    url(r'^date$', views.today_date),
+    url(r'^$', views.home, name='home'),
+    url(r'^article/(?P<article_id>\d+)$', views.read, name='read'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-from datetime import datetime
+from blog.models import Article
 
 # Create your views here.
 def home(request):
-    text = """<h1>Bienvenue sur le site des crepes bretonnes</h1>
-              <p>Les crepes bretonnes ca tue des mouettes en plein vol</p>"""
-    return HttpResponse(text)
+    """List all articles on home page"""
+    articles = Article.objects.all()
+    return render(request, 'blog/home.html', {'last_articles': articles})
 
-def view_article(request, id_article):
-    text = "Vous avez demandé l'article {0}".format(id_article)
-    return HttpResponse(text)
-
-def list_articles(request, year, month):
-    text = "Vous avez demandé la liste des articles du mois {0}/{1}".format(month, year)
-    return HttpResponse(text)
-
-def today_date(request):
-    return render(request, 'blog/date.html', {'today_date':datetime.now()})
+def read(request, article_id):
+    """Display a whole article"""
+    pass
